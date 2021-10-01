f6ec978128af2499427af8545729d41ee3c6c4f7
Paulinho <paulinho@agencia3w.com.br>
2021-10-01 15:25:57 -0300

    Search Page

---
+++ src/components/SearchItem/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+    padding: 14px;
+`;
+
+export const Banner = styled.Image`
+    width: 100%;
+    height: 140px;
+    border-radius: 10px;
+`;
+
+export const RateContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+    padding-top: 10px;
+`;
+
+export const Title = styled.Text`
+    color: #FFFFFF;
+    font-size: 18px;
+    font-weight: bold;
+    padding-top: 10px;
+`;
+
+export const Rate = styled.Text`
+    color: #FFFFFF;
+    font-size: 12px;
+    padding-left: 4px;
+`;
