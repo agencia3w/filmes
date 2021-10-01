ab6d6de3c4296514f48b29c8a5920ef3bcb51a62
Paulinho <paulinho@agencia3w.com.br>
2021-10-01 14:25:26 -0300

    Genres, Modal and Slider components

---
+++ src/components/Genres/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    margin-right: 8px;
+    background-color: #DDDDDD;
+    align-items: center;
+    justify-content: center;
+    padding: 5px 10px;
+    border-radius: 10px;
+`;
+
+export const Name = styled.Text``;
